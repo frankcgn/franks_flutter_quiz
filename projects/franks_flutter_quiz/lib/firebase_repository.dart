5141e71a1dccd189c8b85fc307b695e7cdbe6d22
frank <email@frank-rollmann.de>
2025-02-22 11:06:38 +0100

    firebase support hinzugefügt

--- projects/franks_flutter_quiz/lib/firebase_repository.dart
+++ projects/franks_flutter_quiz/lib/firebase_repository.dart
@@ -3,18 +3,23 @@ import 'models.dart';
 
 class FirebaseRepository {
   final CollectionReference _collection =
-  FirebaseFirestore.instance.collection('vocabularies');
+      FirebaseFirestore.instance.collection('vocabularies');
 
   Future<void> saveOrUpdateVocabulary(Vocabulary voc) async {
-    // Verwende die UUID als Dokument-ID
-    final DocumentReference docRef = _collection.doc(voc.uuid);
-    final DocumentSnapshot docSnapshot = await docRef.get();
-    if (docSnapshot.exists) {
-      // Aktualisieren
-      await docRef.update(voc.toJson());
-    } else {
-      // Neu anlegen
-      await docRef.set(voc.toJson());
+    try {
+      // Verwende die UUID als Dokument-ID
+      final DocumentReference docRef = _collection.doc(voc.uuid);
+      final DocumentSnapshot docSnapshot = await docRef.get();
+      if (docSnapshot.exists) {
+        // Aktualisieren
+        await docRef.update(voc.toJson());
+      } else {
+        // Neu anlegen
+        await docRef.set(voc.toJson());
+      }
+    } catch (e) {
+      print('Fehler beim Zugriff auf Firestore: $e');
+      // Hier kannst du zusätzlich Logik einbauen, z.B. einen Retry-Mechanismus oder eine Benutzerbenachrichtigung.
     }
   }
 }
